717fc89ccc6c888457cf9c516e3545a7688a3be8
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-12-11 08:33:29 +0100

    deleted

--- NetfliAPI/src/services/media/validation.js
+++
@@ -1,6 +0,0 @@
-import { body } from "express-validator"
-
-export const mediaValidation = [
-  body("year").exists().withMessage("Title is a mandatory field!"),
-  body("type").exists().withMessage("Category is a mandatory field!"),
-]
